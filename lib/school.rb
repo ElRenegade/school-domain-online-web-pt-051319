744d93b5b8bb5f9f0939e0a2ebab4453514be82d
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-31 02:14:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,7 +4,7 @@ class School
     @name = name
     @roster = {}
   end
-  def add_student(sutend, grade)
+  def add_student(student, grade)
     roster[grade] = []
     roster[grade] << student
   end
